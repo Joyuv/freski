028f6f157a12d300032a43ce1b4f8da92ef17ee1
Givanilson Geferson Fernandes Mariz <givanilson.fernandes@academico.ifrn.edu.br>
2025-05-05 13:50:45 -0300

    terminado

--- flask2/app.py
+++ flask2/app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("inicial.html")
 
-@app.route('/form', methods=['POST', 'GET'])
-def form():
+@app.route('/cadastro', methods=['POST', 'GET'])
+def cadastro():
     return render_template("cadastro.html")
 
 @app.route('/cookie', methods=['POST'])
 def cookie():
-    response = make_response(redirect(url_for("ver_cookies")))
+    response = make_response(redirect(url_for("preferencias")))
     nome = request.form['nome']
     opc = request.form['genr']
     email = 'email' in request.form
@@ -24,21 +24,40 @@ def cookie():
     response.set_cookie("email", str(email), max_age=semana)
     return response
 
-@app.route('/ver_cookies')
-def ver_cookies():
+@app.route('/preferencias')
+def preferencias():
     if not all(key in request.cookies for key in ['nome', "genero", "email"]):
-        return redirect(url_for("form"))
+        return '<a href="cadastro">Cadastrar</a>' 
     email = request.cookies.get("email")
-    genero = request.cookies.get("genero")
+    if request.cookies.get("genero") == 'comed':
+        genero = "Comédia"
+    elif request.cookies.get("genero") == 'acao':
+        genero = "Ação"
+    elif request.cookies.get("genero") == 'terror':
+        genero = "Terror"
+    elif request.cookies.get("genero") == 'ficcao':
+        genero = "Ficção"
+    else:
+        genero = "Drama"
+    
+    if email == 'True':
+        email = "Sim"
+    else:
+        email = "Não"
     nome = request.cookies.get("nome")
     return render_template('preferencias.html', gen=genero, email=email, nome=nome)
 
 @app.route('/recomendacoes')
 def recomendacoes():
+    filmes = {
+    "Ação": ["John Wick", "Mad Max: Estrada da Fúria", "Missão: Impossível – Efeito Fallout"],
+    "Comédia": ["Superbad", "O Âncora: A Lenda de Ron Burgundy", "As Branquelas"],
+    "Terror": ["Invocação do Mal", "Corra!", "O Exorcista"],
+    "Drama": ["Forrest Gump", "Clube da Luta", "Os Infiltrados"],
+    "Ficção": ["Interestelar", "Matrix", "Blade Runner 2049"]
+    }
     gen = request.args.get('gen', '')
-
-    if gen:
-        resultado = f"recomendações para {gen}"
-    else:
-        return redirect(url_for('index'))
-    return render_template("recomendacoes.html", gen=gen)
+    rec1 = filmes[gen][0]
+    rec2 = filmes[gen][1]
+    rec3 = filmes[gen][2]
+    return render_template("recomendacoes.html", gen=gen, rec1=rec1, rec2=rec2, rec3=rec3)
